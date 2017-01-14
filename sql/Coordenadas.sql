78aa208f33d9316249920e68001a2b222a8b7ee7
patricialoureiro1402 <a_patricia_4@hotmail.com>
2017-01-14 20:51:26 +0000

    links atualizados

--- sql/Coordenadas.sql
+++ sql/Coordenadas.sql
@@ -1,16 +1,7 @@
 DROP TABLE IF EXISTS coordenadas;
 CREATE TABLE coordenadas(
-	id_coordenadas		INTEGER NOT NULL AUTO_INCREMENT,
-	id_viagem			INTEGER NOT NULL,
-    latitude			VARCHAR NOT NULL,
-    longitude			VARCHAR NOT NULL,
-    
-    CONSTRAINT pk_Coordenadas
-		PRIMARY KEY (id_coordenadas),
-        
-	CONSTRAINT fk_Coordenadas_Viagem
-		FOREIGN KEY (id_viagem)
-        REFERENCES viagem(id_viagem)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-)
+	id_coordenadas INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	id_viagem INTEGER NOT NULL,
+    latitude VARCHAR(10) NOT NULL,
+    longitude VARCHAR(10) NOT NULL,
+	CONSTRAINT fk_Coordenadas_Viagem FOREIGN KEY (id_viagem) REFERENCES viagem(id_viagem) ON UPDATE CASCADE ON DELETE CASCADE);
--- sql/Eventos.sql
+++ sql/Eventos.sql
@@ -1,17 +1,8 @@
 DROP TABLE IF EXISTS eventos;
 CREATE TABLE eventos(
-	id_eventos     		INTEGER NOT NULL AUTO_INCREMENT,
-	id_viagem			INTEGER NOT NULL,
-    tipo                VARCHAR(20) NOT NULL,    
-    latitude            VARCHAR NOT NULL,
-    longitude           VARCHAR NOT NULL,
-
-    CONSTRAINT pk_Eventos
-		PRIMARY KEY (id_eventos),
-        
-	CONSTRAINT fk_Eventos_Viagem
-		FOREIGN KEY (id_viagem)
-        REFERENCES viagem(id_viagem)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-)
+	id_eventos INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	id_viagem INTEGER NOT NULL,
+    tipo VARCHAR(20) DEFAULT NULL,    
+    latitude VARCHAR(10) NOT NULL,
+    longitude VARCHAR(10) NOT NULL,
+	CONSTRAINT fk_Eventos_Viagem FOREIGN KEY (id_viagem) REFERENCES viagem(id_viagem) ON UPDATE CASCADE ON DELETE CASCADE);
